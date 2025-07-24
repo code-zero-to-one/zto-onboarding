edeb392ac20fa81ef0f8d3297b86ed0697c0a2d1
이도현 <lucky77795@naver.com>
2025-07-24 15:45:26 +0900

    refactor: SecuritConfig 수정
    기존에 중복되는 부분들과, 권한 관련 설정 한 곳에서 관리하기 위함

---
+++ src/main/java/com/codezerotoone/mvp/global/config/security/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.codezerotoone.mvp.global.config.security;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // Form 로그인 방식 disable
+                .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 인증 방식 disable
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint(
+                                (userInfoEndpointConfig) -> userInfoEndpointConfig.userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 적용
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint()) // 인증 실패 시 동작
+                        .accessDeniedHandler(new CustomAccessDeniedHandler())) //권한 부족 시 동작
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/members")
+                        .hasRole("GUEST")
+                        .requestMatchers("/api/v1/**")
+                        .hasRole("MEMBER")
+                        .requestMatchers("/api/v1/auth/refresh", "/api/auth/check/login")
+                        .permitAll()
+                        .anyRequest().hasAnyRole() // 나머지 경로는 인증 필요
+                )
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .addFilterAfter(new JwtFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+                List.of("http://localhost:3000", "http://localhost:8080")); // 허용할 Origin
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")); // 허용할 HTTP 메서드
+        configuration.setAllowedHeaders(List.of("*")); // 허용할 헤더
+        configuration.setExposedHeaders(List.of("*")); // 노출할 헤더
+        configuration.setAllowCredentials(true); // 인증 정보 포함 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        return source;
+    }
+}
